bf8f0be307576c61d8c9468865726d38583cc93d
Manuel Fonseca <manueldazafon@gmail.com>
2022-05-15 14:54:32 -0500

    estiling button option

--- 01-dialog/src/components/ui/ButtonOption.js
+++ 01-dialog/src/components/ui/ButtonOption.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import styles from './styles.module.css';
 
 const ButtonOption = ({ setType }) => {
     return (
         <>
-            <button onClick={ () => setType("Error")}>Error</button>
-            <button onClick={ () => setType("Success")}>Success</button>
-            <button onClick={ () => setType("Warning")}>Warning</button>
+            <button onClick={ () => setType("Error")} className={styles.buttonOption}>Error</button>
+            <button onClick={ () => setType("Success")} className={styles.buttonOption}>Success</button>
+            <button onClick={ () => setType("Warning")} className={styles.buttonOption}>Warning</button>
         </>
     );
 };
